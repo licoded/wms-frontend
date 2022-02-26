d24d8be60833e092f48b2abb32e471414bfe697e
licoded <busy.li@foxmail.com>
2022-02-26 10:49:27 +0800

    feat(layout): init

---
+++ src/layouts/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout-wrapper">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import {
+  defineComponent,
+} from 'vue';
+
+export default defineComponent({
+  name: 'LayoutView',
+  setup() {
+    return {
+    };
+  },
+});
+</script>
+
