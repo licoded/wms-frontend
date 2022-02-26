c42c209e461632ce6d3bb030a16db578affd05c1
licoded <busy.li@foxmail.com>
2022-02-27 01:13:19 +0800

    feat(stylelint): install extends and config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "extends": "stylelint-config-standard",
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-standard-vue/scss",
+  ],
   "rules": {
     "function-linear-gradient-no-nonstandard-direction": null,
   }
