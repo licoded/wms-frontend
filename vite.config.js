7c34fe85d1c787e64ca56394e53a8e0cb80ec35a
licoded <busy.li@foxmail.com>
2022-02-27 16:40:20 +0800

    fix(config): tsconfig.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,37 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
+/* eslint-enable */
+
+function optimizedInclude() {
+  return [
+    'vue',
+    'axios',
+    'vue-router',
+    'vue-i18n',
+    '@intlify/vite-plugin-vue-i18n',
+  ];
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueI18n({
-      include: path.resolve(__dirname, './src/locales/locales/**'),
+      include: path.resolve(__dirname, './src/i18n/locales/**'),
     }),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
-    extensions: ['.js', '.jsx', 'json'],
+    extensions: ['.js', '.jsx', 'json', 'css', 'scss', 'vue'],
+  },
+  optimizeDeps: {
+    include: optimizedInclude(),
+    exclude: [],
+  },
+  build: {
+    sourcemap: true,
   },
 });
