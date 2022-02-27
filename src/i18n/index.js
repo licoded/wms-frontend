6c2ab5046046a8cd6cd7cd97e1b8ffb9041b0dfb
licoded <busy.li@foxmail.com>
2022-02-27 15:55:48 +0800

    feat(i18n): config int vite.config.js

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,29 +1,13 @@
 import { createI18n } from 'vue-i18n';
+import enUS from './locales/en-US.json';
+import zhCN from './locales/zh-CN.json';
 
 const i18n = createI18n({
   legacy: false,
   locale: 'zh-CN',
   messages: {
-    'zh-CN': {
-      language: '中文',
-      login: {
-        btn: '登录',
-        forget: '忘记密码？',
-        pwd: '密码',
-        tip: '用户登录',
-        username: '用户名',
-      },
-    },
-    'en-US': {
-      language: 'English',
-      login: {
-        btn: 'Sign in',
-        forget: 'Forget Password?',
-        pwd: 'Password',
-        tip: 'Please sign in',
-        username: 'Username',
-      },
-    },
+    'zh-CN': zhCN,
+    'en-US': enUS,
   },
 });
 
